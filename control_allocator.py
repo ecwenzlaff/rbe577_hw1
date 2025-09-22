956da6f86a9e533b1658e79888952be25513fd4c
Everett <ecwenzlaff@wpi.edu>
2025-09-21 22:56:44 -0400

    Incorporated initial random walk and sample clamping capabilities. Also optimized the force to torque transform via a vmap workaround module.

--- control_allocator.py
+++ control_allocator.py
@@ -3,9 +3,10 @@
 # ecwenzlaff@wpi.edu
 
 import sys
-from typing import Any  # specified types (esp. those using "Any") primarily convey intention and are not guaranteed to work with static type checking libraries 
+from typing import Any, Tuple # specified types (esp. those using "Any") primarily convey intention and are not guaranteed to work with static type checking libraries 
 import numpy as np
 import torch
+import vmap_workaround as vw
 import matplotlib.pyplot as plt
 
 # Global Variables and Parameters:
@@ -17,7 +18,26 @@ alpha2_bounds = (-180.0, 180.0)
 alpha3_bounds = (-180.0, 180.0)
 l1, l2, l3, l4 = -14.0, 14.5, -2.7, 2.7
 
-def forces2torques(forcetensor: torch.FloatTensor) -> torch.FloatTensor:    # input dimensions: [5,N], output dimensions: [3,N]
+def randomWalkTensor(tensorsize: Tuple[int, int], maxstepvector: torch.FloatTensor) -> torch.FloatTensor:
+    # Input/output formatting anticipates state variables in rows and sampled states in columns:
+    sample_stepsizes = torch.rand(tensorsize, dtype=torch.float32).to(device)                      # generates floats from 0 to 1.0
+    sample_stepsizes[:,0] = 0.0
+    sign_tensor = torch.randint(low=-1, high=2, size=tensorsize, dtype=torch.float32).to(device)    # generates ints from [low, high)
+    #sign_tensor[torch.where(sign_tensor == 0.0)] = -1.0    # use this when [low, high) is [0, 2) to keep samples in "constant motion"
+    scaled_steps = (maxstepvector.to(device) * (sign_tensor * sample_stepsizes)).to(device)    # element-wise multiplication to scale steps forward or backward
+    return torch.cumsum(scaled_steps, dim=1)
+
+def clampSamples(tensorin: torch.FloatTensor) -> torch.FloatTensor:
+    # TODO: instead of doing a simple clamp, it might be more advantageous to "redraw a step" in the random walk. If you go
+    # down this route, it might be better to call this function from the randomWalkTensor function and add additional inputs 
+    # to this function to account for random walk parameters/constraints.
+    tensorout = torch.zeros((tensorin.shape[0], tensorin.shape[1]), dtype=torch.float32).to(device)
+    bound_vector = [F1_bounds, F2_bounds, F3_bounds, alpha2_bounds, alpha3_bounds]
+    for i in range(0, len(bound_vector)):
+        tensorout[i,:] = torch.clamp(tensorin[i,:], bound_vector[i][0], bound_vector[i][1])
+    return tensorout
+
+def forcesToTorques(forcetensor: torch.FloatTensor) -> torch.FloatTensor:    # input dimensions: [5,N], output dimensions: [3,N]
     transform_tensor = torch.zeros((forcetensor.shape[1],5,5), dtype=torch.float32).to(device)
     #torque_tensor = torch.zeros((5,forcetensor.shape[1]), dtype=torch.float32).to(device)
     # Each 5x5xi slice should align with the torque transform in section 3.1. So when the 3x3
@@ -39,40 +59,33 @@ def forces2torques(forcetensor: torch.FloatTensor) -> torch.FloatTensor:    # in
     transform_tensor[:,2,1] = l1*torch.sin((np.pi/180)*forcetensor[3,:].reshape(-1)) - l3*torch.cos((np.pi/180)*forcetensor[3,:].reshape(-1))
     transform_tensor[:,2,2] = l1*torch.sin((np.pi/180)*forcetensor[4,:].reshape(-1)) - l4*torch.cos((np.pi/180)*forcetensor[4,:].reshape(-1))
     # Even though I only care about a single slice, broadcasting here could blow up my GPU memory,
-    # so I'll unfortunately need to use list comprehension instead...
+    # so I'll need to try using vmap or list comprehension to apply the 5x5 transform to each 5x1 column...
     # torque_tensor = (transform_tensor @ forcetensor)[0,0:3,0:3] 
-    #def inplacetransform(i):
-    #    torque_tensor[:,i] = torch.matmul(transform_tensor[i,:,:], forcetensor[:,i])
-    #[torque_tensor[i,:] = (transform_tensor[i,:,:] @ forcetensor[:,i]) for i in range(0,forcetensor.shape[1])]
-    #torch.vmap(inplacetransform)(torch.arange(0,forcetensor.shape[1]))
-    torque_tensor = torch.stack([transform_tensor[i,:,:] @ forcetensor[:,i] for i in range(0,forcetensor.shape[1])]).T[0:3,:].to(device)
-    return torque_tensor
+    def inplacetransform(i: torch.Tensor) -> torch.Tensor:
+        return torch.matmul(transform_tensor[i,:,:], forcetensor[:,i])
+    torque_tensor = vw.vmap(inplacetransform)(torch.arange(0,forcetensor.shape[1])).to(device)
+    #torque_tensor = torch.stack([transform_tensor[i,:,:] @ forcetensor[:,i] for i in range(0,forcetensor.shape[1])]).T[0:3,:].to(device)
+    return torque_tensor[:,0:3,0].T
 
+# TODO: need to implement the Encoder/Decoder neural net architecture
+
+# TODO: need to incorporate one of pytorch's data loaders somewhere to split data into (train, validation, test) sets
 
 if __name__ == '__main__':
     print(f"Python Version = {sys.version}, Torch Device = {device}")
-    sampled_tensor = torch.rand((5,int(1e6)), dtype=torch.float32).to(device)   # should generate numbers from -1.0 to 1.0
-    scale_tensor = torch.tensor([
-        F1_bounds[1], F2_bounds[1], F3_bounds[1], alpha2_bounds[1], alpha3_bounds[1]
-        ], dtype=torch.float32).reshape(-1,1).to(device)
-    sign_tensor = torch.randint(low=0, high=2, size=(5,int(1e6)), dtype=torch.float32).to(device)
-    sign_tensor[torch.where(sign_tensor == 0.0)] = -1.0
-    test_tensor = torch.zeros((5,int(1e6)), dtype=torch.float32).to(device) 
-    # Desired element-wise operation sequence: (1) each scale_tensor element multiplied by the associated row in sampled_tensor, 
-    # followed by (2) each element in sign_tensor multiplied by each element in sampled_tensor
-    test_tensor = sign_tensor * (scale_tensor * sampled_tensor) 
-    #print(f"{test_tensor = }, {test_tensor.shape = }")
-    torque_tensor = forces2torques(test_tensor)
+    test_tensor = randomWalkTensor((5,int(1e6)), torch.tensor([20, 5, 5, 0.5, 0.5]).reshape(-1,1)).to(device)
+    test_tensor = clampSamples(test_tensor)
+    torque_tensor = forcesToTorques(test_tensor)
     
     # Plot everything:
-    F1_samples = test_tensor.cpu().detach().numpy()[0,:].reshape(-1,1)
-    F2_samples = test_tensor.cpu().detach().numpy()[1,:].reshape(-1,1)
-    F3_samples = test_tensor.cpu().detach().numpy()[2,:].reshape(-1,1)
-    alpha2_samples = test_tensor.cpu().detach().numpy()[3,:].reshape(-1,1)
-    alpha3_samples = test_tensor.cpu().detach().numpy()[4,:].reshape(-1,1)
-    tau1_samples = torque_tensor.cpu().detach().numpy()[0,:].reshape(-1,1)
-    tau2_samples = torque_tensor.cpu().detach().numpy()[1,:].reshape(-1,1)
-    tau3_samples = torque_tensor.cpu().detach().numpy()[2,:].reshape(-1,1)
+    F1_samples = test_tensor.cpu().detach().numpy()[0,:].reshape(-1,1)#[0:300,:]
+    F2_samples = test_tensor.cpu().detach().numpy()[1,:].reshape(-1,1)#[0:300,:]
+    F3_samples = test_tensor.cpu().detach().numpy()[2,:].reshape(-1,1)#[0:300,:]
+    alpha2_samples = test_tensor.cpu().detach().numpy()[3,:].reshape(-1,1)#[0:300,:]
+    alpha3_samples = test_tensor.cpu().detach().numpy()[4,:].reshape(-1,1)#[0:300,:]
+    tau1_samples = torque_tensor.cpu().detach().numpy()[0,:].reshape(-1,1)#[0:300,:]
+    tau2_samples = torque_tensor.cpu().detach().numpy()[1,:].reshape(-1,1)#[0:300,:]
+    tau3_samples = torque_tensor.cpu().detach().numpy()[2,:].reshape(-1,1)#[0:300,:]
     fig_F1, ax_F1 = plt.subplots(1,1)
     fig_F2, ax_F2 = plt.subplots(1,1)
     fig_F3, ax_F3 = plt.subplots(1,1)
@@ -81,20 +94,20 @@ if __name__ == '__main__':
     fig_tau1, ax_tau1 = plt.subplots(1,1)
     fig_tau2, ax_tau2 = plt.subplots(1,1)
     fig_tau3, ax_tau3 = plt.subplots(1,1)
-    ax_F1.plot(np.arange(F1_samples.shape[0]), F1_samples, '.')
+    ax_F1.plot(np.arange(F1_samples.shape[0]), F1_samples, '.-')
     ax_F1.set_title("F1")
-    ax_F2.plot(np.arange(F2_samples.shape[0]), F2_samples, '.')
+    ax_F2.plot(np.arange(F2_samples.shape[0]), F2_samples, '.-')
     ax_F2.set_title("F2")
-    ax_F3.plot(np.arange(F3_samples.shape[0]), F3_samples, '.')
+    ax_F3.plot(np.arange(F3_samples.shape[0]), F3_samples, '.-')
     ax_F3.set_title("F3")
-    ax_alpha2.plot(np.arange(alpha2_samples.shape[0]), alpha2_samples, '.')
+    ax_alpha2.plot(np.arange(alpha2_samples.shape[0]), alpha2_samples, '.-')
     ax_alpha2.set_title("Alpha2")
-    ax_alpha3.plot(np.arange(alpha3_samples.shape[0]), alpha3_samples, '.')
+    ax_alpha3.plot(np.arange(alpha3_samples.shape[0]), alpha3_samples, '.-')
     ax_alpha3.set_title("Alpha3")
-    ax_tau1.plot(np.arange(tau1_samples.shape[0]), tau1_samples, '.')
+    ax_tau1.plot(np.arange(tau1_samples.shape[0]), tau1_samples, '.-')
     ax_tau1.set_title("Tau1")
-    ax_tau2.plot(np.arange(tau2_samples.shape[0]), tau2_samples, '.')
+    ax_tau2.plot(np.arange(tau2_samples.shape[0]), tau2_samples, '.-')
     ax_tau2.set_title("Tau2")
-    ax_tau3.plot(np.arange(tau3_samples.shape[0]), tau3_samples, '.')
+    ax_tau3.plot(np.arange(tau3_samples.shape[0]), tau3_samples, '.-')
     ax_tau3.set_title("Tau3")
     plt.show()
---
+++ vmap_workaround.py
@@ -0,0 +1,70 @@
+# RBE 577 - Machine Learning for Robotics
+# HW 1: Control Allocation via Deep Neural Networks
+# ecwenzlaff@wpi.edu
+
+from functools import wraps
+from typing import Any, Sequence, Callable # specified types (esp. those using "Any") primarily convey intention and are not guaranteed to work with static type checking libraries 
+import torch
+from torch.overrides import TorchFunctionMode
+
+# This module defines a helper class and wrapper for working around the torch.vmap issue documented in
+# https://github.com/pytorch/pytorch/issues/124423. This workaround was initially taken from
+# https://github.com/EMI-Group/evox/blob/edb94acf2bcae2d054c2ada455d30504976b85cc/src/evox/core/module.py#L154
+# and built out further to handle additional edge cases. This module should ultimately provide a direct 
+# replacement for torch.vmap:
+
+def _transform_scalar_index(ori_index: Sequence[Any | torch.Tensor] | Any | torch.Tensor):
+    if isinstance(ori_index, Sequence):
+        index = tuple(ori_index)
+    else:
+        index = (ori_index,)
+    any_scalar_tensor = False
+    new_index = []
+    for idx in index:
+        if isinstance(idx, torch.Tensor) and idx.ndim == 0:
+            new_index.append(idx[None])
+            any_scalar_tensor = True
+        elif isinstance(idx, slice) and isinstance(idx.stop, torch.Tensor) and idx.stop.ndim == 0:  # NOTE: found really weird case where the underlying input tensor is buried in a slice
+            new_index.append(idx.stop[None])
+            any_scalar_tensor = True
+        else:
+            new_index.append(idx)
+    if not isinstance(ori_index, Sequence):
+        new_index = new_index[0]
+    if type(new_index) == type([]):
+        new_index = tuple(new_index)    # NOTE: added conversion from List to Tuple here to avoid UserWarning about using a non-tuple sequence for multidimensional indexing
+    return new_index, any_scalar_tensor  
+
+class TransformGetSetItemToIndex(TorchFunctionMode):
+    # This is needed since we want to support calling
+    # A[idx] or A[idx] += b, where idx is a scalar tensor.
+    # When idx is a scalar tensor, Torch implicitly convert it to a python
+    # scalar and create a view of A.
+    # Workaround: We convert the scalar tensor to a 1D tensor with one element.
+    # That is, we convert A[idx] to A[idx[None]][0], A[idx] += 1 to A[idx[None]] += 1.
+    # This is a temporary solution until the issue is fixed in PyTorch.
+    def __torch_function__(self, func, types, args, kwargs=None):
+        # A[idx]
+        if func == torch.Tensor.__getitem__:
+            x, index = args
+            new_index, any_scalar = _transform_scalar_index(index)
+            x = func(x, new_index, **(kwargs or {}))
+            if any_scalar:
+                x = x.squeeze(0)
+            return x
+        # A[idx] = value
+        elif func == torch.Tensor.__setitem__:
+            x, index, value = args
+            new_index, _ = _transform_scalar_index(index)
+            return func(x, new_index, value, **(kwargs or {}))
+        return func(*args, **(kwargs or {}))
+    
+@wraps(torch.vmap)
+def vmap(*args, **kwargs) -> Callable:
+    # Fixes the `torch.vmap`'s issue with __getitem__ and __setitem__.
+    # Related issue: https://github.com/pytorch/pytorch/issues/124423.
+    vmapped = torch.vmap(*args, **kwargs)
+    def wrapper(*args, **kwargs):
+        with TransformGetSetItemToIndex():
+            return vmapped(*args, **kwargs)
+    return wrapper
